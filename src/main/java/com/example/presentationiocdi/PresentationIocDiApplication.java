7178b45da675505a45d2fbcda7f321338b80b770
Roman Dyriv <xephosroman@gmail.com>
2022-02-01 16:51:00 +0200

    Initial commit

---
+++ src/main/java/com/example/presentationiocdi/PresentationIocDiApplication.java
@@ -0,0 +1,13 @@
+package com.example.presentationiocdi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PresentationIocDiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PresentationIocDiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/presentationiocdi/PresentationIocDiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.presentationiocdi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PresentationIocDiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
